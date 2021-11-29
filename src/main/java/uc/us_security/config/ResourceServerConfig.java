0721b0f8827ed7b99f941ea94f83356e64e3ffdc
b3nj4hb <benjalive12@gmail.com>
2021-11-29 11:20:01 -0500

    sin restricciones
    permitir todos los metodos get

--- src/main/java/uc/us_security/config/ResourceServerConfig.java
+++ src/main/java/uc/us_security/config/ResourceServerConfig.java
@@ -22,7 +22,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/uc").permitAll()
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll()
 		.anyRequest().authenticated();
 	}
 	@Bean
